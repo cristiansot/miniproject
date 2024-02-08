6bb117d38c4f166b5eeb91a62e3b0c14097c3ff2
cristiansot <soluciongrafix@gmail.com>
2024-02-08 11:15:49 -0300

    GET, GET id, POST and DELETE method done

--- routes/endpoints.js
+++ routes/endpoints.js
@@ -22,42 +22,40 @@ let currencies = [
 	}
 ]
 
-router.get('/', (request, response) => {
-	response.json('Hello world') 
+router.get('/', (req, res) => {
+	res.json('Hello world') 
 })
   
-router.get('/api/currency/', (request, response) => {
-	response.json(currencies)
+router.get('/api/currency/', (req, res) => {
+	res.json(currencies)
+  console.log('Showing currencies')
 })
   
-router.get('/api/currency/:id', (request, response) => {
-	const id = Number(request.params.id);
-	const currencyId = currencies.find(currencyId => currencyId.id === id); 
+router.get('/api/currency/:id', (req, res) => {
+	const id = Number(req.params.id);
+	const currencyId = currencies.find(x => x.id === id); 
 
 	if (currencyId) { 
-		response.json(currencyId);
+		res.json(currencyId);
 	} else if (currencyId !== id){
-		response.status(404).json({ error: 'Resource not found' });
+		res.status(404).json({ error: 'Resource not found' });
 	} 
 })
-  
-router.post('/api/currency/', (request, response) => {
-	const newCurrencie = {
-		// id: 4,
-		// currencyCode: "AFN",
-		// country: "Afghanistan",
-		// conversionRate: "971",
-	}
-	
 
-	if (newCurrencie.id != 4) {
-		response.status(404).json({ error: 'content missing' });
-	} else {
-		response.json(newCurrencie);
-	}
-})
-  
+router.post('/api/currency/', (req, res) => {
+  try {
+    const newCurrencies =  req.body;
+    newCurrencies.id = currencies.length + 1,
+    currencies.push(newCurrencies);
+    res.status(201).json(newCurrencies);
+    console.log('Currency created');
+  } catch (error){
+    res.status(500).json({ message: error.message})  }
+});
+
 router.put('/api/currency/:id/:newRate', (request, response) => {
+  const id = Number(req.params.id);
+
 	const newRate = {
 		id: 5,
 		currencyCode: "GNF",
@@ -66,11 +64,19 @@ router.put('/api/currency/:id/:newRate', (request, response) => {
 	}
 	response.json(newRate);
 })
-  
-router.delete('/api/currency/del/:id', (request, response) => {
-	const id = Number(request.params.id);
-	const currencyDel = currencies.filter(currencyId => currencyId.id != id);
-	response.json(currencyDel);
-})
+
+router.delete('/api/currency/:id', (req, res) => {
+	const id = Number(req.params.id);
+	const initialLength = currencies.length;
+
+	currencies = currencies.filter(currency => currency.id !== id);
+	if (currencies.length < initialLength) {
+	  console.log('Currency deleted');
+	  res.status(201).send({ message: 'Currency deleted' });
+	} else {
+	  console.log('Currency not found');
+	  res.status(404).send({ message: 'Currency not found' });
+	}
+});
   
 module.exports = router;
