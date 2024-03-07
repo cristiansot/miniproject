007768a7153630c80483e1a327bd730b537a2d75
cristiansot <soluciongrafix@gmail.com>
2024-03-07 15:47:26 -0300

    Test folder created and added inside currency.utils and currency.test

---
+++ client/src/test/currency_utils.js
@@ -0,0 +1,19 @@
+/**
+ * This file is meant to be where you will complete the utility function below, for performing a conversion of some amount
+ * to another currency.
+ */
+
+/**
+ * TODO:
+ * @receives two currency objects, currencyA and currencyB, as well as an integer-amount of cash in currencyA
+ * @performs a currency conversion between integer amount of currencyA to an integer amount of currencyB
+ * @returns an integer
+ */
+const convertCurrency = (currencyA, currencyB, amount) => {
+  let newAmount = (amount / currencyA) * currencyB;
+  newAmount = Math.ceil(newAmount);
+  // console.log(newAmount);
+  return newAmount
+}
+
+module.exports = convertCurrency
